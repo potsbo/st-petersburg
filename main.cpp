2fbb0a1aebb2ededa8e165041e066e00b966723c
Shimpei Otsubo <potsbo@gmail.com>
2017-10-24 22:07:37 +0900

    Remove quotes

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ void run(double n) {
 	for(int i = 0; i < max; i++)
 		sum += trial();
 	
-	cout << "\"" << sum / (double) max << "\",\"" << n << "\"" << endl;
+	cout << sum / (double) max  << "," << n << endl;
 }
 
 int main(void) {
