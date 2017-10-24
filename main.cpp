53e84f2f9e1800947a5564fb365f78dc0365face
Shimpei Otsubo <potsbo@gmail.com>
2017-10-24 21:44:49 +0900

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int trial(void) {
+	for(int i = 0;; i++)
+		if (rand()%2)
+			return pow(2, i-1);
+}
+
+void run(double n) {
+	long long max = pow(2, n);
+
+	long long sum = 0;
+	for(int i = 0; i < max; i++)
+		sum += trial();
+	
+	cout << "\"" << sum / (double) max << "\",\"" << n << "\"" << endl;
+}
+
+int main(void) {
+	srand((unsigned) time(NULL));
+
+	int max = 30;
+	double step = 0.5;
+
+	for(int n = 0; n < max / step; n++){
+		run(n * step);
+	}
+	return 0;
+}
